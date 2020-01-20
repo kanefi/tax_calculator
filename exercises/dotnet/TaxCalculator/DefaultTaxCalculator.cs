de3599be81fd28be16613c0254c75d36cfc691be
FKANE <FKANE@SBWT618303>
2020-01-20 13:36:10 +0000

    tests for petrol

--- exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
@@ -8,7 +8,60 @@ namespace TaxCalculator
     {
         public override int CalculateTax(Vehicle vehicle)
         {
-            return 0;
+            if (vehicle.Co2Emissions == 0)
+            {
+                return 0;
+            }
+
+            else if (vehicle.Co2Emissions <= 50)
+            {
+                return 10;
+            }
+            else if (vehicle.Co2Emissions <= 75)
+            {
+                return 25;
+            }
+            else if (vehicle.Co2Emissions <= 89)
+            {
+                return 105;
+            }
+            else if (vehicle.Co2Emissions <= 99)
+            {
+                return 105;
+            }
+            else if (vehicle.Co2Emissions <= 109)
+            {
+                return 125;
+            }
+            else if (vehicle.Co2Emissions <= 129)
+            {
+                return 145;
+            }
+            else if (vehicle.Co2Emissions <= 149)
+            {
+                return 165;
+            }
+            else if (vehicle.Co2Emissions <= 169)
+            {
+                return 205;
+            }
+            else if (vehicle.Co2Emissions <= 189)
+            {
+                return 515;
+            }
+            else if (vehicle.Co2Emissions <= 224)
+            {
+                return 830;
+            }
+            else if (vehicle.Co2Emissions <= 254)
+            {
+                return 1240;
+            }
+            else if (vehicle.Co2Emissions <= 255)
+            {
+                return 1760;
+                }
+            else return 2070;
         }
     }
 }
